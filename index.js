4ce81c2c5612342ca56f995f7fcfc3aba0700d8c
EDLLT <47005435+EDLLT@users.noreply.github.com>
2022-07-05 18:10:09 +0300

    Init

---
+++ index.js
@@ -0,0 +1,33 @@
+var heading = document.querySelector("h1");
+
+var firstPlayerDiceImage = document.querySelector("#dice1-img")
+var secondPlayerDiceImage = document.querySelector("#dice2-img")
+
+
+function random(range) {
+  return Math.floor(Math.random() * range) + 1
+}
+
+function shuffleDice(diceImg) {
+  var newNumber = random(6);
+  diceImg.setAttribute("src", "D:\\Desktop\\Web\\Dicee Challenge - Starting Files\\images\\dice"+ newNumber + ".png")
+
+  return newNumber;
+}
+
+function displayWinner(newHeadingText) {
+  heading.textContent = newHeadingText;
+}
+
+firstDiceNumber = shuffleDice(firstPlayerDiceImage)
+secondDiceNumber = shuffleDice(secondPlayerDiceImage)
+
+if (firstDiceNumber > secondDiceNumber) {
+  displayWinner("🚩 Player 1 wins! ")
+} else if (secondDiceNumber > firstDiceNumber) {
+  displayWinner("Player 2 wins! 🚩")
+} else if (secondDiceNumber === firstDiceNumber) {
+  displayWinner("Draw!")
+} else {
+  displayWinner("WTF")
+}
